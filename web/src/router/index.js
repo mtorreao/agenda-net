f69324fbd23cfce586714dfdd8a5e76aee672fda
Matheus Torreão <mtorreao1@gmail.com>
2023-01-15 14:17:09 -0300

    Adicionando projeto Vue para o frontend

---
+++ web/src/router/index.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/sign-in',
+      name: 'sign-in',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/SignInView.vue')
+    },
+    {
+      path: '/sign-up',
+      name: 'sign-up',
+      component: () => import('../views/SignUpView.vue')
+    }
+  ]
+})
+
+export default router
---
+++ web/src/views/SignUpView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Cadastro</h1>
+  </div>
+</template>
+
+<style>
+
+</style>
